c6bc7127f4120e6757d897feb379f7c82ca35219
M G <drowdemon@gmail.com>
2013-05-20 19:09:18 -0400

    Completed add to self. UNTESTED!!

--- PointGenerator/main.cpp
+++ PointGenerator/main.cpp
@@ -27,7 +27,7 @@ public:
 	}
 };
 
-class EllipticCurve
+class EllipticCurve //y^2=x^3+a2x^2+a4x+a6
 {
 public:
 	int a2;
@@ -157,7 +157,7 @@ EllipticCurve init()
 	return E;
 }
 
-void addToSelf(EllipticCurve E, Point gen)
+Point addToSelf(EllipticCurve E, Point gen) // returns 2p.
 {
 	mpq_t slope;
 	mpq_init(slope);
@@ -165,20 +165,43 @@ void addToSelf(EllipticCurve E, Point gen)
 	mpq_init(p1);
 	mpq_init(p2);
 
-	mpz_mul_si(mpq_numref(p1),mpq_numref(gen.x),E.a2); //a*x1 == first coef*numerator of x
-	mpz_mul(mpq_numref(p1),mpq_numref(p1),mpq_denref(gen.y)); //ans*y2 == ans * denominator of y
+	mpz_mul_si(mpq_numref(p1),mpq_numref(gen.x),3); //3x_1
+	mpz_mul(mpq_numref(p1),mpq_numref(p1),mpq_numref(gen.x)); //3x_1^2
+	mpz_mul(mpq_numref(p1),mpq_numref(p1),mpq_denref(gen.y)); //3x_1^2y_2 == ans*y2 == ans * denominator of y
 
-	mpz_mul(mpq_denref(p1),mpq_numref(gen.y),mpq_denref(gen.x));
+	mpz_mul(mpq_denref(p1),mpq_numref(gen.y),mpq_denref(gen.x)); //x_2y_1
+	mpz_mul_si(mpq_denref(p1),mpq_denref(p1),2); //2*x_2y_1
 
-	mpq_canonicalize(p1);
+	mpq_canonicalize(p1); //first fraction complete
 
-	mpz_mul_si(mpq_numref(p1),mpq_denref(gen.y),E.a4);
-	mpz_mul_si(mpq_denref(p1),mpq_numref(gen.y),2);
+	mpz_mul_si(mpq_numref(p1),mpq_denref(gen.y),E.a4); //Ay_2
+	mpz_mul_si(mpq_denref(p1),mpq_numref(gen.y),2); //2y_1
 
+	mpq_canonicalize(p2); //second fraction complete
+
+	mpq_add(slope,p1,p2); //add
+	//mpq_clear(p1);
+	//mpq_clear(p2);
+	//slope is now correct //Re-using variables because allocation is slow.
+	mpq_set(p1,slope); //sets p1=slope
+	mpq_set(p2,gen.x); //sets p1=slope
+	mpq_mul(p1,slope,slope); //p1=slope^2
+	mpz_mul_si(mpq_numref(p2),mpq_numref(gen.x),2); //changing the given point: multiplication by 2: alpha=2alpha
 	mpq_canonicalize(p2);
+	mpq_sub(slope,p1,p2); //slope-=2alpha
+	//net effect right now, from original slope, p1=slope^2-2alpha
+	//p2 is useless, again. p1 contains the correct x value - to be returned.
+	mpq_sub(p2,p1,gen.x); //p2=p1-x ... x-alpha
+	mpq_mul(p2,p2,slope); //p2=slope*p2... p2=slope(p1-alpha)
+	mpq_add(p2,p2,gen.y); //p2=p2+gen.y ... p2=slope(x-alpha)+beta == y coordinate.
+	//curve is symmetric about the y axis. We want the other point.
+	mpq_neg(p2,p2);
 
-	mpq_add(slope,p1,p2);
+	Point ret;
+	mpq_set(ret.x,p1);
+	mpq_set(ret.y,p2);
+	mpq_clear(slope);
 	mpq_clear(p1);
 	mpq_clear(p2);
-	//slope is now correct
+	return ret;
 }

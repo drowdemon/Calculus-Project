95c210354877bfa4b2d9f602427aa5a4b1b8c4a3
M G <drowdemon@gmail.com>
2013-05-21 16:10:33 -0400

    Completed addition of 2 points. UNTESTED.

--- PointGenerator/main.cpp
+++ PointGenerator/main.cpp
@@ -184,12 +184,12 @@ Point addToSelf(EllipticCurve E, Point gen) // returns 2p.
 	//mpq_clear(p2);
 	//slope is now correct //Re-using variables because allocation is slow.
 	mpq_set(p1,slope); //sets p1=slope
-	mpq_set(p2,gen.x); //sets p1=slope
+	mpq_set(p2,gen.x); //sets p2=gen.x
 	mpq_mul(p1,slope,slope); //p1=slope^2
 	mpz_mul_si(mpq_numref(p2),mpq_numref(gen.x),2); //changing the given point: multiplication by 2: alpha=2alpha
 	mpq_canonicalize(p2);
-	mpq_sub(slope,p1,p2); //slope-=2alpha
-	//net effect right now, from original slope, p1=slope^2-2alpha
+	mpq_sub(p1,p1,p2); //p1=p1-p2: p1=slope^2-2alpha
+	//slope is unchanged
 	//p2 is useless, again. p1 contains the correct x value - to be returned.
 	mpq_sub(p2,p1,gen.x); //p2=p1-x ... x-alpha
 	mpq_mul(p2,p2,slope); //p2=slope*p2... p2=slope(p1-alpha)
@@ -203,5 +203,37 @@ Point addToSelf(EllipticCurve E, Point gen) // returns 2p.
 	mpq_clear(slope);
 	mpq_clear(p1);
 	mpq_clear(p2);
+	return ret;
+}
+
+Point add(EllipticCurve E, Point gen1, Point gen2) // returns gen1+gen2
+{
+	mpq_t slope;
+	mpq_init(slope);
+	mpq_t p1;
+	mpq_init(p1);
+
+	mpq_sub(slope,gen2.y,gen1.y); //numerator of slope: slope=y2-y1
+	mpq_sub(p1,gen2.x,gen1.x); //denominator of slope: p1=x2-x1
+	mpq_div(slope,slope,p1); //slope=slope/p1, proper slope
+	//mpq_clear(p1);
+	//slope is now correct //Re-using variables because allocation is slow.
+	mpq_set(p1,slope); //sets p1=slope
+	mpq_mul(p1,slope,slope); //p1=slope^2
+	mpq_sub(p1,p1,gen1.x);
+	mpq_sub(p1,p1,gen2.x); //p1=slope^2-alpha_1-alpha_2
+	//net effect right now, from original slope, p1=slope^2-2alpha - correct x value
+	//p2 is useless, again. p1 contains the correct x value - to be returned.
+	Point ret;
+	mpq_set(ret.x,p1); //p1 stored - x val stored
+	mpq_sub(p1,p1,gen1.x); //p1=p1-x ... x-alpha
+	mpq_mul(p1,p1,slope); //p1=slope*p1... p1=slope(x-alpha)
+	mpq_add(p1,p1,gen1.y); //p2=p2+gen.y ... p1=slope(x-alpha)+beta == y coordinate.
+	//curve is symmetric about the y axis. We want the other point.
+	mpq_neg(p1,p1);
+
+	mpq_set(ret.y,p1);
+	mpq_clear(slope);
+	mpq_clear(p1);
 	return ret;
 }
